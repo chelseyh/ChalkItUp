a78cccfd5632563c5ddb3aed4ff6eb4c142b1510
Chelsey Hudson <ch0621973@otc.edu>
2020-11-11 09:48:43 -0600

    trying chalk

---
+++ app-Chelsey’s MacBook Pro-2.js
@@ -0,0 +1,1 @@
+const chalkAnimation = require('chalk-animation');
---
+++ app2.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk');
+const chalkAnimation = require('chalk-animation');
+const log = console.log
+
+log(chalk.bgCyan('There once was a very lazy doberman. . . '))
